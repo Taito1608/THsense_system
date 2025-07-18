398d1c8fa1a0c69faf58ce5fe2dc0ae38c7681b1
Taito1608 <taitoyusa.0816@gmail.com>
2025-07-18 16:10:22 +0900

    [update] Arduinoからデータを受信する関数に説明を追加

--- function_pkg/data_receive_from_arduino.py
+++ function_pkg/data_receive_from_arduino.py
@@ -1,19 +1,24 @@
 import serial
 
+# ポートの設定
+# 注意: ポート名は環境に応じて変更してください
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
+# Arduinoからのデータを受信する関数
 def read_usb():
   in_txt=ser.readline()
   while ser.in_waiting:
     in_txt = in_txt + ser.readline()
   print("")
-  print( '    Input_Text: ', in_txt )
+  print( 'Input_Text: ', in_txt )
 
+  # 受信したデータを分割
   txt1 = in_txt.split(b':')
   txt2 = txt1[0].split(b'Temperature=')
   txt3 = txt1[1].split(b'Humidity=')
   txt4 = txt3[1].split(b'\r')
 
+  # 文字列から数値に変換
   temp = float( txt2[1] )
   humid = float( txt3[1] )
 
