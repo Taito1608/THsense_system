f67629f97bfad0cb9e711f4a012f66092e4bd6cb
Taito1608 <taitoyusa.0816@gmail.com>
2025-07-20 01:20:38 +0900

    [fix] DBに接続する際のconnectionの設定を関数内に定義

--- function_pkg/data_save_db.py
+++ function_pkg/data_save_db.py
@@ -16,21 +16,27 @@ def put_data_record( temp,humid):
   db_table = os.getenv("DB_TABLE")  # テーブル名を取得
   print( 'ID=%s  DateTime=%s  Temp=%.2f  Humidity=%.2f' %(id, dt, temp, humid) )
   try:
+    connection = pymysql.connect(
+       host=db_ip_addr,
+       user=os.getenv("DB_USER"),
+       password=os.getenv("DB_PASSWORD"),
+       db=os.getenv("DB_NAME"), charset="utf8"
+       )
     with connection.cursor() as cursor:
       sql = "INSERT INTO "+db_table+" (id, dt, temp, humid) VALUES(%s, %s, %s, %s)"
       cursor.execute( sql, (id, dt, temp, humid))
     connection.commit()
     print("    Data commited.")
-  except:
-    print("DB Access Error!")
+  except Exception as e:
+        print("DB Access Error!", e)
+  finally:
+      if 'connection' in locals():  # connectionが存在する場合のみclose
+          connection.close()
 
 if __name__ == '__main__':
-    connection = pymysql.connect(host=db_ip_addr, user=os.getenv("DB_USER"), password=os.getenv("DB_PASSWORD"), db=os.getenv("DB_NAME"), charset="utf8")
     try:
         temp = float(input("Enter Temperature: "))
         humid = float(input("Enter Humidity: "))
         put_data_record(temp, humid)
     except KeyboardInterrupt:
         pass
-    finally:
-        connection.close()
