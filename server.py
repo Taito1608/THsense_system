bec0174ef70962c4672f847d10679628d711885d
Taito1608 <taitoyusa@icloud.com>
2025-07-21 00:55:41 +0900

    [update] 現在の最新の温度をブラウザに表示する機能追加

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
+from function_pkg.data_get_db import get_now_data, get_hour_data
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
-def display_csvdata():
-    print("Hello! (to Terminal)")
+def display_lastdata():
+    print("display_lastdataを実行しました。(to Terminal)")
+
+    # 最新のデータを取得
+    last_data = get_now_data()
     
-    return render_template("index.html") 
+    return render_template("index.html",last_data=last_data) 
 
 # Flaskアプリケーションを起動
 if __name__ == "__main__":
