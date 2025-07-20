40f355d937c89784a61101c45cc8445a44180753
Taito1608 <taitoyusa@icloud.com>
2025-07-21 01:37:14 +0900

    [add] グラフをブラウザに表示する機能追加

--- server.py
+++ server.py
@@ -12,6 +12,21 @@ def display_lastdata():
     
     return render_template("index.html",last_data=last_data) 
 
+@app.route("/hour", methods=["GET"])
+def display_hourdata():
+    print("display_hourdataを実行しました。(to Terminal)")
+
+    # 過去60件分のデータを取得
+    hour_data = get_hour_data()
+
+    # データを整形してグラフ用に準備
+    graph_data = [
+        [dt.strftime("%Y %m/%d %H:%M"), temp, hum]
+        for _, dt, temp, hum in hour_data
+    ]
+    
+    return render_template("hour.html", graph_data=graph_data)
+
 # Flaskアプリケーションを起動
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
